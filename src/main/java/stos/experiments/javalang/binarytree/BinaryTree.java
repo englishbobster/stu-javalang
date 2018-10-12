24e29c191a61310b849f8adc8b225660dec492b4
Stuart Osborne <stuart.osborne@infinera.com>
2018-10-12 16:15:02 +0200

    Insert,Delete added to BinaryTree, and tests.

--- src/main/java/stos/experiments/javalang/binarytree/BinaryTree.java
+++ src/main/java/stos/experiments/javalang/binarytree/BinaryTree.java
@@ -2,17 +2,93 @@ package stos.experiments.javalang.binarytree;
 
 class BinaryTree {
 
-    TreeNode rootNode;
+    TreeNode rootNode = null;
 
-    BinaryTree() {
-        this.rootNode = new TreeNode(0);
-    }
+    BinaryTree() { }
 
-    int getRootValue() {
-        return rootNode.getValue();
-    }
-
+    /**
+     * - If the new node’s value is lower than the current node’s, we go to the left child
+     * - If the new node’s value is greater than the current node’s, we go to the right child
+     * - When the current node is null, we’ve reached a leaf node and we can insert the new node in that position
+     */
     void insert(int value) {
-        rootNode.setValue(value);
+        rootNode = insertRecursive(rootNode, value);
+    }
+
+    boolean contains(int value) {
+        return containsNodeRecursive(rootNode, value);
+    }
+
+    private TreeNode insertRecursive(TreeNode current, int value) {
+        if (current == null) {
+            current = new TreeNode(value);
+        }
+        if (value < current.value) {
+            current.left = insertRecursive(current.left, value);
+        } else if (value > current.value) {
+            current.right = insertRecursive(current.right, value);
+        }
+        return current;
+    }
+
+    private boolean containsNodeRecursive(TreeNode current, int value) {
+        if (current == null) {
+            return false;
+        }
+        if (current.value == value) {
+            return true;
+        }
+        if (current.value < value) {
+            return containsNodeRecursive(current.right, value);
+        } else {
+            return containsNodeRecursive(current.left, value);
+        }
+    }
+
+    void delete(int value) {
+        rootNode = deleteRecursive(rootNode, value);
+    }
+
+    private TreeNode deleteRecursive(TreeNode current, int value) {
+        if (current == null) {
+            return null;
+        }
+        if (value == current.value) {
+            if (current.left == null && current.right == null) {
+                return null;
+            }
+            if (current.right == null) {
+                return current.left;
+            }
+            if (current.left == null) {
+                return current.right;
+            }
+            int smallestValue = findSmallestValue(current.right);
+            current.value = smallestValue;
+            current.right = deleteRecursive(current.right, smallestValue);
+            return current;
+        }
+        if (current.value > value) {
+            current.left = deleteRecursive(current.left, value);
+            return current;
+        }
+        current.right = deleteRecursive(current.right, value);
+        return current;
+    }
+
+    private int findSmallestValue(TreeNode current) {
+        return current.left == null ? current.value :findSmallestValue(current.left);
+    }
+
+    private class TreeNode {
+        int value;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int value) {
+            this.value = value;
+            left = null;
+            right = null;
+        }
     }
 }
--- src/test/java/stos/experiments/javalang/binarytree/BinaryTreeTest.java
+++ src/test/java/stos/experiments/javalang/binarytree/BinaryTreeTest.java
@@ -1,24 +1,41 @@
 package stos.experiments.javalang.binarytree;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 class BinaryTreeTest {
 
-    @Test
-    public void check_that_a_new_binary_tree_has_a_root_node() {
-        BinaryTree tree = new BinaryTree();
-        assertThat(tree.getRootValue(), is(equalTo(0)));
+    private static BinaryTree TREE;
+    private static final int DELETE_VALUE = 88;
+    private static final int[] TEST_VALUES = new int[]{1, 4, 7, 2, 8, 10, 67, 3, 6, DELETE_VALUE, 33, 44};
+
+    @BeforeEach
+    void setUp() {
+        TREE = new BinaryTree();
+        for (int value : TEST_VALUES) {
+            TREE.insert(value);
+        }
     }
 
     @Test
-    public void check_that_we_can_insert_value_into_root_node() {
-        BinaryTree tree = new BinaryTree();
-        tree.insert(10);
-        assertThat(tree.getRootValue(), is(equalTo(10)));
+    void check_that_a_new_binary_tree_has_a_new_node_when_inserting() {
+        for (int value : TEST_VALUES) {
+            assertThat(TREE.contains(value), is(true));
+        }
     }
 
+    @Test
+    void check_that_a_new_binary_tree_does_not_have_a_value() {
+        assertThat(TREE.contains(100), is(false));
+    }
+
+    @Test
+    void check_that_an_entry_is_deleted_from_the_tree() {
+        assertThat(TREE.contains(DELETE_VALUE), is(true));
+        TREE.delete(DELETE_VALUE);
+        assertThat(TREE.contains(DELETE_VALUE), is(false));
+    }
 }
